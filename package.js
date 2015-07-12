5f0bed9854f3f725b895a69d1dbe0688151ed954
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-07-12 13:42:29 +0530

    v1.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Server Side Router for Meteor",
-  "version": "1.0.2",
+  "version": "1.0.3",
   "git": "https://github.com/meteorhacks/picker.git",
   "name": "meteorhacks:picker"
 });
